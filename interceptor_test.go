dcf82cc14d1254b8e6b737367f8a83ad6c42a5bd
Ox Cart <ox.to.a.cart@gmail.com>
2016-10-18 17:48:18 -0500

    More tests

--- interceptor_test.go
+++ interceptor_test.go
@@ -2,6 +2,7 @@ package interceptor
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
 	"github.com/getlantern/httptest"
 	"github.com/getlantern/mockconn"
@@ -32,32 +33,77 @@ func TestDialFailure(t *testing.T) {
 }
 
 func TestCONNECT(t *testing.T) {
-	op := ops.Begin("TestPipe")
+	doTest(t, ops.Begin("TestCONNECT"), "CONNECT", true, true)
+}
+
+func TestPipeKeepFirst(t *testing.T) {
+	doTest(t, ops.Begin("TestPipeKeepFirst"), "GET", true, true)
+}
+
+func TestPipeDontKeepFirst(t *testing.T) {
+	doTest(t, ops.Begin("TestPipeDontKeepFirst"), "GET", true, false)
+}
+
+// func TestHTTPKeepFirst(t *testing.T) {
+// 	doTest(t, ops.Begin("TestPipeKeepFirst"), "GET", true, true)
+// }
+
+func doTest(t *testing.T, op ops.Op, requestMethod string, pipe bool, forwardInitialRequest bool) {
 	defer op.End()
-	d := mockconn.SucceedingDialer([]byte("Response"))
-	w := httptest.NewRecorder([]byte("Request"))
+	nestedReq, _ := http.NewRequest("POST", "http://subdomain.thehost/stuff", ioutil.NopCloser(bytes.NewBuffer([]byte("My Request"))))
+	nestedReqText := dumpRequest(nestedReq)
+	resp := httptest.NewRecorder(nil)
+	resp.WriteHeader(http.StatusCreated)
+	resp.Write([]byte("My Response"))
+	respText := dumpResponse(resp.Result())
+	d := mockconn.SucceedingDialer([]byte(respText))
+	w := httptest.NewRecorder([]byte(nestedReqText))
 	i := New(&Opts{
 		Dial: func(req *http.Request, addr string, port int) (net.Conn, bool, error) {
 			log.Debug(addr)
 			conn, err := d.Dial("tcp", addr)
-			return conn, true, err
+			return conn, pipe, err
 		},
 	})
-	req, _ := http.NewRequest("CONNECT", "http://thehost", nil)
-	i.Intercept(w, req, false, op, 756)
+	req, _ := http.NewRequest(requestMethod, "http://thehost", nil)
+	i.Intercept(w, req, forwardInitialRequest, op, 756)
 	assert.Equal(t, "thehost:756", d.LastDialed(), "Should have defaulted port to 756")
 	r := bufio.NewReader(w.Body())
-	resp, err := http.ReadResponse(r, nil)
-	if !assert.NoError(t, err) {
-		return
+	isConnect := requestMethod == "CONNECT"
+	if isConnect {
+		resp, err := http.ReadResponse(r, nil)
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
 	}
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
 	responseData, err := ioutil.ReadAll(r)
 	if !assert.NoError(t, err) {
 		return
 	}
-	assert.Equal(t, "Response", string(responseData))
-	assert.Equal(t, "Request", string(d.Received()))
+	assert.Equal(t, string(respText), string(responseData))
+	received := bufio.NewReader(bytes.NewBuffer(d.Received()))
+	if !isConnect && forwardInitialRequest {
+		recReq, err := http.ReadRequest(received)
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, dumpRequest(req), dumpRequest(recReq), "Should have forwarded initial request")
+	}
+	remainingReceived, _ := ioutil.ReadAll(received)
+	assert.Equal(t, nestedReqText, string(remainingReceived), "Should have received piped data from downstream")
 	assert.True(t, w.Closed(), "Downstream connection not closed")
 	assert.True(t, d.AllClosed(), "Upstream connection not closed")
+}
+
+func dumpRequest(req *http.Request) string {
+	buf := &bytes.Buffer{}
+	req.Write(buf)
+	return buf.String()
+}
+
+func dumpResponse(resp *http.Response) string {
+	buf := &bytes.Buffer{}
+	resp.Write(buf)
+	return buf.String()
 }
